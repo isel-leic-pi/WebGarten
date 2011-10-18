19bef79f2ada5213fa7a73972c2acf9362d70431
Pedro Felix <pmhsfelix@gmail.com>
2011-10-18 12:11:36 +0100

    Undo accidental regression of HttpResponse.cs

--- WebGarten/PI.WebGarten/HttpResponse.cs
+++ WebGarten/PI.WebGarten/HttpResponse.cs
@@ -5,11 +5,27 @@ using System.Text;
 
 namespace PI.WebGarten
 {
+    using System;
+    using System.Reflection;
+
     public class HttpResponse
     {
         private readonly int _status;
         private readonly IHttpContent _content;
         private IDictionary<string, string> _headers;
+
+        private static readonly ISet<string> _HackedHeaders = new HashSet<string> { "WWW-Authenticate" };
+
+        private static readonly MethodInfo _AddInternalMethodInfo;
+
+        static HttpResponse()
+        {
+            _AddInternalMethodInfo = typeof(WebHeaderCollection).GetMethod("AddInternal", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (_AddInternalMethodInfo == null)
+            {
+                throw new Exception("Hacking HttpListener resctricted headers not supported");
+            }
+        }
 
         public HttpResponse WithHeader(string name, string value)
         {
@@ -28,7 +44,7 @@ namespace PI.WebGarten
             _status = status;
         }
 
-        public HttpResponse(HttpStatusCode status) : this((int)status){}
+        public HttpResponse(HttpStatusCode status) : this((int)status) { }
 
         public HttpResponse(int status, IHttpContent content)
         {
@@ -36,7 +52,7 @@ namespace PI.WebGarten
             _content = content;
         }
 
-        public HttpResponse(HttpStatusCode status, IHttpContent content):this((int)status, content){}
+        public HttpResponse(HttpStatusCode status, IHttpContent content) : this((int)status, content) { }
 
         public void Send(HttpListenerContext c)
         {
@@ -52,13 +68,26 @@ namespace PI.WebGarten
             if (_headers == null) return;
             foreach (var p in _headers)
             {
-                resp.Headers[p.Key] = p.Value;
+                AddHeaderInternal(p.Key, p.Value, resp.Headers);
             }
+        }
+
+        private static void AddHeaderInternal(string key, string value, WebHeaderCollection headers)
+        {
+            if (_HackedHeaders.Contains(key))
+            {
+                _AddInternalMethodInfo.Invoke(headers, new object[] { key, value });
+            }
+            else
+            {
+                headers[key] = value;
+            }
+
         }
 
         private void SendContent(HttpListenerResponse resp)
         {
-            if(_content == null) return;
+            if (_content == null) return;
             var ms = new MemoryStream();
             var w = new StreamWriter(ms, Encoding.UTF8);
             resp.ContentType = _content.ContentType + "; charset=utf-8";
@@ -68,7 +97,7 @@ namespace PI.WebGarten
             ms.Seek(0, SeekOrigin.Begin);
             using (var cs = resp.OutputStream)
             {
-                ms.CopyTo(cs);    
+                ms.CopyTo(cs);
             }
         }
     }
